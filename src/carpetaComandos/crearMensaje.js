3e77139a17023a34d6286054f340ba5b86d3f670
przybylski46 <przybylski4637@gmail.com>
2026-08-27 20:35:15 -0600

    Refactor crearMensaje.js to simplify command structure

--- src/carpetaComandos/crearMensaje.js
+++ src/carpetaComandos/crearMensaje.js
@@ -1,17 +1,6 @@
-const {
-  SlashCommandBuilder,
-  ContainerBuilder,
-  TextDisplayBuilder,
-  ActionRowBuilder,
-  StringSelectMenuBuilder,
-  StringSelectMenuOptionBuilder,
-  ButtonBuilder,
-  ButtonStyle
-} = require('discord.js');
-
-const {
-  crearConstructor
-} = require('../constructor/estado');
+const { SlashCommandBuilder } = require('discord.js');
+const { crearConstructor } = require('../constructor/estado');
+const { crearInterfaz } = require(('../constructor/interfaz');
   
 module.exports = {
   data: new SlashCommandBuilder()
@@ -19,62 +8,10 @@ module.exports = {
   .setDescription('Crea un mensaje usando Components V2'),
 
   async execute(interaction) {
-
-    crearConstructor(interaction.user.id);
-
-    const contenedor = new ContainerBuilder()
-    .setAccentColor(8237567)
-    .addTextDisplayComponents(
-      new TextDisplayBuilder()
-      .setContent(
-        '## 🛠️ Constructor\n' +
-        '> **Empieza añadiendo componentes a tu mensaje**'
-        )
-      );
-
-    const menu = new StringSelectMenuBuilder()
-    .setCustomId('constructor_add')
-    .setPlaceholder('Añadir componente')
-    .addOptions(
-      new StringSelectMenuOptionBuilder()
-      .setLabel('Contenedor')
-      .setValue('contenedor')
-      .setEmoji('📦'),
-      new StringSelectMenuOptionBuilder()
-      .setLabel('Botones')
-      .setValue('botones')
-      .setEmoji('🖱️')
-      );
-
-    const filaMenu = new ActionRowBuilder()
-    .addComponents(menu);
-    const filaBotones = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setCustomId('constructor_vista_previa')
-      .setEmoji('👁️')
-      .setStyle(ButtonStyle.Primary),
-      new ButtonBuilder()
-      .setCustomId('constructor_editar')
-      .setEmoji('✏️')
-      .setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder()
-      .setCustomId('constructor_exportar')
-      .setEmoji('📄')
-      .setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder()
-      .setCustomId('constructor_cancelar')
-      .setEmoji('🚽')
-      .setStyle(ButtonStyle.Danger)
-      );
-    
+    const estado = crearConstructor(interaction.user.id);
     await interaction.reply({
       flags: 32768,
-      components: [
-        contenedor,
-        filaMenu,
-        filaBotones
-        ]
+      components: crearInterfaz(estado)
     });
   }
 };
