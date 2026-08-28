7fff832ab34ed18f7658e0dee6cbbc94db4e6a95
przybylski46 <przybylski4637@gmail.com>
2026-08-28 02:03:07 -0600

    Add modal creation function for text input

---
+++ src/constructor/modals/texto.js
@@ -0,0 +1,29 @@
+const {
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder
+} require('discord.js');
+
+function crearModalTexto(indice) {
+  
+  const modal = new ModalBuilder()
+  .setCustomId(`constructor_texto_${indice}`)
+  .setTitle('📝 Añadir texto');
+  
+  const texto= new TextInputBuilder()
+  .setCustomId('contenido')
+  .setLabel('Contenido')
+  .setPlaceholder('Escribe sin hacer ruido para no despertar al Spinosaurus...')
+  .setStyle(TextInputStyle.Paragraph)
+  .setRequired(true);
+  
+  const fila = new ActionRowBuilder()
+  .addComponents(texto);
+  modal.addComponents(fila);
+  return modal;
+}
+
+module.exports = {
+  crearModalTexto
+};
