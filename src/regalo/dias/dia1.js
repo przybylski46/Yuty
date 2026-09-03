479dc5577ae10ed5dff58bf8587cc366d5bce5db
przybylski46 <przybylski4637@gmail.com>
2026-09-02 20:12:25 -0600

    Implement crearDia1 function for Day 1 surprise

--- src/regalo/dias/dia1.js
+++ src/regalo/dias/dia1.js
@@ -1,1 +1,34 @@
+const {
+    ContainerBuilder,
+    TextDisplayBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder
+} = require('discord.js');
 
+function crearDia1() {
+
+    const container = new ContainerBuilder()
+        .addTextDisplayComponents(
+            new TextDisplayBuilder().setContent(
+                '# 🌿 Una pequeña sorpresa\n\n' +
+                'Hay lugares que empiezan estando completamente vacíos...\n\n' +
+                'hasta que llega alguien y les da una razón para existir.\n\n' +
+                '**Día 1 de 7**'
+            )
+        )
+        .addActionRowComponents(
+            new ActionRowBuilder().addComponents(
+                new ButtonBuilder()
+                    .setCustomId('regalo_dia1_abrir')
+                    .setLabel('🌱 Abrir')
+                    .setStyle(ButtonStyle.Success)
+            )
+        );
+
+    return container;
+}
+
+module.exports = {
+    crearDia1
+};
