c07c3964b5e70cff30ba53efc7b46ccd1a798476
przybylski46 <przybylski4637@gmail.com>
2026-09-02 20:42:38 -0600

    Refactor command registration for global and server use

--- src/registrarComandos.js
+++ src/registrarComandos.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 
 const { REST, Routes } = require('discord.js');
-
 const fs = require('node:fs');
-
 const path = require('node:path');
 
 const ID_SERVIDOR_REGALO = '1541553721505357876';
 
-const comandos = [];
+const comandosGlobales = [];
+const comandosServidor = [];
 
 const rutaComandos = path.join(__dirname, 'carpetaComandos');
-
 const archivosComandos = fs.readdirSync(rutaComandos).filter(archivo => archivo.endsWith('.js'));
 
 for (const archivo of archivosComandos) {
@@ -25,12 +23,20 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log(`Registrando ${comandos.length} comando(s)...`);
+        console.log(`Registrando ${comandosGlobales.length} comando(s) global(s)...`);
 
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
-            { body: comandos }
+            { body: comandosGlobales }
         );
+        console.log(`Registrando ${comandosServidor.length} comando(s) en el servidor...`);
+        await rest.put(
+            Routes.applicationCommands(
+                process.env.CLIENT_ID,
+                ID_SERVIDOR_REGALO
+                ),
+            { body: comandosServidor }
+            );
 
         console.log('Comandos registrados correctamente.');
     } catch (error) {
