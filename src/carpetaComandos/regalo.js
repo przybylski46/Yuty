2a13c3dd95fa1e48e9a1f6d54f09533f6dbd0015
przybylski46 <przybylski4637@gmail.com>
2026-09-02 19:47:06 -0600

    Update gift command to use specific user ID

--- src/carpetaComandos/regalo.js
+++ src/carpetaComandos/regalo.js
@@ -1,22 +1,21 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-const ID_PERSONA_REGALO = 'PON_AQUI_EL_ID';
+const ID_REGALO = '880352642700812308';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('regalo')
         .setDescription('Hay una pequeña sorpresa para ti...'),
-
+    
     async execute(interaction) {
-
-        if (interaction.user.id !== ID_PERSONA_REGALO) {
+        if (interaction.user.id !== ID_REGALO) {
             await interaction.reply({
                 content: 'Este regalo no es para ti 👀',
                 ephemeral: true
             });
             return;
         }
-
+        
         await interaction.reply({
             content: '🌿 **Día 1 de 7**\n\nHay una pequeña sorpresa para ti...',
             ephemeral: true
