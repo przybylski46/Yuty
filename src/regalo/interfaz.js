3002711163a5d77f95e88ffa3932177282159685
przybylski46 <przybylski4637@gmail.com>
2026-09-02 20:12:51 -0600

    Add crearInterfazRegalo function for gift interface

--- src/regalo/interfaz.js
+++ src/regalo/interfaz.js
@@ -1,1 +1,14 @@
+const { crearDia1 } = require('./dias/dia1');
 
+function crearInterfazRegalo(estado) {
+
+    if (estado.dia === 1) {
+        return [crearDia1()];
+    }
+
+    return [];
+}
+
+module.exports = {
+    crearInterfazRegalo
+};
