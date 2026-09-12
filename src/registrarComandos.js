03dbd913bf5aa46d87e0ecba92991506ba483fb5
przybylski46 <przybylski4637@gmail.com>
2026-09-11 18:21:21 -0600

    Refactor command registration and logging messages

--- src/registrarComandos.js
+++ src/registrarComandos.js
@@ -10,35 +10,40 @@ const comandosGlobales = [];
 const comandosServidor = [];
 
 const rutaComandos = path.join(__dirname, 'carpetaComandos');
-const archivosComandos = fs.readdirSync(rutaComandos).filter(archivo => archivo.endsWith('.js'));
+const archivosComandos = fs.readdirSync(rutaComandos)
+    .filter(archivo => archivo.endsWith('.js'));
 
 for (const archivo of archivosComandos) {
     const rutaArchivo = path.join(rutaComandos, archivo);
     const comando = require(rutaArchivo);
 
-    comandos.push(comando.data.toJSON());
+    if (comando.guildOnly) {
+        comandosServidor.push(comando.data.toJSON());
+    } else {
+        comandosGlobales.push(comando.data.toJSON());
+    }
 }
 
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log(`Registrando ${comandosGlobales.length} comando(s) global(s)...`);
+        console.log(`Registrando ${comandosGlobales.length} comando(s) global(es)...`);
 
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
             { body: comandosGlobales }
-        );
-        console.log(`Registrando ${comandosServidor.length} comando(s) en el servidor...`);
+            );
+        console.log (`Registrando ${comandosServidor.length} comando(s) en el servidor...`);
+
         await rest.put(
-            Routes.applicationCommands(
+            Routes.applicationGuildCommands(
                 process.env.CLIENT_ID,
                 ID_SERVIDOR_REGALO
                 ),
             { body: comandosServidor }
             );
-
-        console.log('Comandos registrados correctamente.');
+        console.log('Comandos registrados correctamente');
     } catch (error) {
         console.error(error);
     }
